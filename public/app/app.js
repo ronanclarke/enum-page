d60a8c80b87d6441affc6c48af444c294d8af5a6
Ronan Clarke <ronanclarke@gmail.com>
2016-05-25 06:59:31 +0100

    switched to backbone cos familiar

--- public/app/app.js
+++ public/app/app.js
@@ -1,18 +1,42 @@
 (function ($) {
 
+  var app = app || {}
+  app.views = {}
+  app.collections = {}
+  app.models = {}
+
+  var allData = Backbone.Collection.extend({
+    model: itemModel
+  });
+  var itemModel = Backbone.Model.extend({
+
+  });
+
   //
   // this view manages the searching and updating of the selected results collection
   //
   var SearchView = Backbone.View.extend({
     el: $('#searchView'),
 
+
     initialize: function () {
       _.bindAll(this, 'render');
+
 
       this.render();
     },
 
     render: function () {
+
+      var data = window.data;
+      console.log(data)
+      allData = new Backbone.Collection(data);
+
+      console.log(allData.length);
+
+
+
+
       $(this.el).append("<ul> <li>hello world</li> </ul>");
     }
   });
